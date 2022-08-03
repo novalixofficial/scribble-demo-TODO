238fed1e408514f835e0a6e9ef6634374ba4abdd
Alexandre Weisser <alexandre.weisser@gmail.com>
2022-08-03 20:21:11 +0200

    Update config.py

--- server/config.py
+++ server/config.py
@@ -1,7 +1,13 @@
+import datetime
 import os
 
-SERVER_NAME = "localhost:8080"
-HOST = "localhost"
+SERVER_NAME = "dev.localhost:8080"
+SESSION_COOKIE_DOMAIN = "dev.localhost"
+SESSION_COOKIE_SAMESITE = "None"
+SESSION_COOKIE_SECURE = False
+
+CORS_HEADERS = 'Content-Type'
+HOST = "dev.localhost"
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -20,7 +26,6 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
 
 SESSION_PERMANENT = True
 SESSION_TYPE = 'sqlalchemy'
-PERMANENT_SESSION_LIFETIME = 60 * 60
-
-SMALL_FILE_SIZE_LIMIT = 20 * 1000   # 1 000 = 1ko
+PERMANENT_SESSION_LIFETIME = datetime.timedelta(hours=2)
+SMALL_FILE_SIZE_LIMIT = 20 * 1000  # 1 000 = 1ko
 FILE_SIZE_LIMIT = 5 * 1000 * 1000  # 1 000 000 = 1Mo
